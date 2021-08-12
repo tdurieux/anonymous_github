b36cad0cbc20f747b458699c0df34e0241b2db0c
tdurieux <durieuxthomas@hotmail.com>
2021-08-12 11:23:58 +0200

    add configuration for storage

--- config.ts
+++ config.ts
@@ -23,6 +23,7 @@ interface Config {
   S3_CLIENT_SECRET?: string;
   S3_ENDPOINT?: string;
   S3_REGION?: string;
+  STORAGE: "filesystem" | "s3";
 }
 const config: Config = {
   CLIENT_ID: "CLIENT_ID",
@@ -51,7 +52,14 @@ const config: Config = {
     "sol",
     "in",
   ],
+  STORAGE: "filesystem",
+  S3_BUCKET: null,
+  S3_CLIENT_ID: null,
+  S3_CLIENT_SECRET: null,
+  S3_ENDPOINT: null,
+  S3_REGION: null,
 };
+
 for (let conf in process.env) {
   if ((config as any)[conf] !== undefined) {
     (config as any)[conf] = process.env[conf];
