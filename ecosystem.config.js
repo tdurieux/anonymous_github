b20d1ab0b14d6a5a6d5446e085ef1d9719dec70a
tdurieux <durieuxthomas@hotmail.com>
2021-09-07 09:17:25 +0200

    chore: do not watch repository folder

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,9 @@ module.exports = {
       script: "./index.ts",
       exec_mode: "fork",
       watch: true,
+      ignore_watch: ["node_modules", "repositories", "repo"],
       interpreter: "node",
-      interpreter_args:
-        "--require ts-node/register",
+      interpreter_args: "--require ts-node/register",
     },
   ],
 };
