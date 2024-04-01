d2aa5d6361265c75b546346e1ee7c51088f4de55
tdurieux <durieuxthomas@hotmail.com>
2024-04-01 06:43:51 +0100

    refactor: use replicas instead of node forks

--- index.ts
+++ index.ts
@@ -1,16 +1,7 @@
-const cluster = require('node:cluster');
 import { config } from "dotenv";
 config();
 
 import server from "./src/server";
 
-if (cluster.isPrimary) {
-  console.log(`Master process ${process.pid} is running`);
-
-  for (let i = 0; i < 8; i++) {
-    cluster.fork();
-  }
-} else {
-  // start the server
-  server();
-}
+// start the server
+server();
